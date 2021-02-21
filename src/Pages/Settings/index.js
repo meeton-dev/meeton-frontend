3f954aa049df2ba17d7efa155a0e04f12767371a
Nikoletta Kotora <nikoletta.kotora@gmail.com>
2021-02-21 19:12:10 +0000

    create radiobutton and radiobutton group

--- src/Pages/Settings/index.js
+++ src/Pages/Settings/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RadioBtnGroup } from '../../Components/Forms/RadioButtonGroup';
+
+const langArray = [
+    {
+        id: "en",
+        value: "en",
+        label: "english",
+        isSelected: true
+    },
+    {
+        id: "de",
+        value: "de",
+        label: "german",
+        isSelected: false
+    },
+    {
+        id: "hu",
+        value: "hu",
+        label: "hungarian",
+        isSelected: false
+    }
+]
 
 const Settings = () => {
+
+    const [languages,  setLanguages] = useState(langArray);
+
+    const handleLanguageChange = (value) => {
+        setLanguages(prevState => {
+            // console.log(prevState);
+            const newState = [...prevState];
+            newState.map(object => {
+                if(object.value === value) {
+                    object.isSelected = true
+                } else {
+                    object.isSelected = false
+                }
+
+            })
+            return newState;
+
+        })
+    }
+
 // modalVisible
     return (
         <section className='Settings'>
             Settings page
+            <RadioBtnGroup data={languages} onChange={handleLanguageChange}/>
         </section>
     );
 };
