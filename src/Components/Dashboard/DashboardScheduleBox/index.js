693e0fc5e3abafd7de21d0cab89a3d906baf2b2e
Nikoletta Kotora <nikoletta.kotora@gmail.com>
2021-03-02 22:30:58 +0000

    some changes in settings css

--- src/Components/Dashboard/DashboardScheduleBox/index.js
+++ src/Components/Dashboard/DashboardScheduleBox/index.js
@@ -3,9 +3,19 @@ import React, { useRef, useEffect, useState } from "react";
 const DashboardScheduleBox = () => {
   return (
     <>
-      DashboardScheduleBox
+    <div classname='dashboardScheduleBox'>
+      <div className='dashboardScheduleBox-title'>Now running</div>
+      <div classname='dashboardScheduleBox-description'></div>
+      <div classname='dashboardScheduleBox-title'>Next meeting(s)</div>
+      <div classname='dashboardScheduleBox-row'></div>
+      <div classname='dashboardScheduleBox-row'></div>
+    </div>
+    
+    <div></div>
+    <div></div>
+    <div></div>
     </>
-  );
+  )
 };
 
 export default DashboardScheduleBox;
